7d8a882d58b8c7a80d3deff195941318052b66aa
Harshil0514 <harshilgehlot1405@gmail.com>
2025-11-03 21:44:45 +0530

    Final fix for SHAP plot using explicit matplotlib figure

--- 1_Kubera_Assessor.py
+++ 1_Kubera_Assessor.py
@@ -156,15 +156,20 @@ if st.button("Assess Risk"):
             shap_values_for_class_1 = shap_values_object.values[0,:,1]
             base_value_for_class_1 = shap_values_object.base_values[0,1]
 
-            # NEW: Create the plot as a ForceVisualizer object
-            # We use shap.plots.force (the underlying function)
-            plot_object = shap.plots.force(
+            # NEW: Use st.pyplot() but create the figure first.
+            # This is the most reliable way for matplotlib plots.
+            fig, ax = plt.subplots(figsize=(10, 2.5)) # Create a figure and axis
+            
+            shap.force_plot(
                 base_value=base_value_for_class_1,
                 shap_values=shap_values_for_class_1,
-                features=scaled_features_df.iloc[0] # Pass the first (and only) row as a Series
+                features=scaled_features_df.iloc[0], # Pass the first (and only) row
+                matplotlib=True, # Tell shap to use matplotlib
+                show=False,      # Don't try to show it
+                ax=ax            # Tell shap which axis to draw on
             )
             
-            # NEW: Render the plot by calling its .html() method
-            components.html(plot_object.html(), height=150, scrolling=True)
+            st.pyplot(fig, bbox_inches='tight') # Pass the FIGURE to st.pyplot
+            plt.close(fig) # Close the figure to save memory
             
             st.caption("These are the SHAP values for the 'Probability of Bankruptcy' (Class 1). Features pushing the score higher (to 'High Risk') are in red. Features pushing lower are in blue.")
